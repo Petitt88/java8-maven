4af960f116d0aa727c304aaa9d4752f8ba92d9ed
pkongyik <petitt88@gmail.com>
2016-05-08 04:41:48 +0200

    jsp messing around

--- webapp-stock/WebContent/Views/home/about.jsp
+++ webapp-stock/WebContent/Views/home/about.jsp
@@ -1,1 +1,33 @@
 <div>Hello from the about page under home section.</div>
+
+<%
+	//request.getParameter("sdsd");
+	//session.getAttribute(("ss");
+	//config.getInitParameter("sdsd");
+	//application.getAttribute("sds");
+
+	//pageContext.getAttribute("edsd")
+
+	// equivalent as the application.setAttribute
+	//pageContext.setAttribute("param", "value", PageContext.APPLICATION_SCOPE);
+
+	// will check in different scopes (page, request, session, application)
+	//pageContext.findAttribute("sdsdd");
+
+	// comes from the web.xml
+	//String initParam = getInitParameter("param1");
+	String initParam = getServletConfig().getInitParameter("param1");
+%>
+
+<%=initParam%>
+
+<%=getServletContext().getAttribute("over")%>
+
+
+<%!/*
+	 tomcat generates such a method when compiling this into a servlet
+	 we can override it
+	*/
+	public void jspInit() {
+		getServletContext().setAttribute("over", "overrinding the jspInit method");
+	}%>
--- webapp-stock/src/org/pet/servlets/HomeServlet.java
+++ webapp-stock/src/org/pet/servlets/HomeServlet.java
@@ -5,17 +5,20 @@ import java.io.PrintWriter;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.pet.helpers.Calculator;
+
 /**
  * Servlet implementation class HomeServlet
  */
-@WebServlet("/home")
-// @WebServlet(urlPatterns = { "/home" })
+// @WebServlet("/home")
+@WebServlet(urlPatterns = { "/home" }, initParams = { @WebInitParam(name = "car", value = "Peugeot") })
 public class HomeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -47,6 +50,18 @@ public class HomeServlet extends HttpServlet {
 		// global store (app-level)
 		ServletContext context = request.getServletContext();
 		// context.setAttribute(arg0, arg1);
+
+		String car = this.getServletConfig().getInitParameter("car");
+		writer.println("Car from ServletConfig is: " + car);
+		
+		
+
+		this.calculateAndWrite(writer);
+	}
+
+	private void calculateAndWrite(PrintWriter writer) {
+		String calced = new Calculator().calc();
+		writer.println(String.format("calculated: %s", calced));
 	}
 
 }
