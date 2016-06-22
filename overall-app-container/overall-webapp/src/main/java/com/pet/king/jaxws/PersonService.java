f1385523b0e985322eac54b93ba2738c673a3587
pkongyik <petitt88@gmail.com>
2016-06-22 04:59:38 +0200

    JAX-WS: additional method on service contract interface

--- overall-app-container/overall-webapp/src/main/java/com/pet/king/jaxws/PersonService.java
+++ overall-app-container/overall-webapp/src/main/java/com/pet/king/jaxws/PersonService.java
@@ -5,19 +5,19 @@ import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 import javax.jws.soap.SOAPBinding.Style;
 
-//@WebService
-//@SOAPBinding(style = Style.RPC)
+// @WebService
+// @SOAPBinding(style = Style.RPC)
 public interface PersonService {
 
-	//@WebMethod
+	// @WebMethod
 	String Hello();
 
-	// boolean addPerson(Person p);
+	boolean addPerson(Person p);
 
-	// boolean deletePerson(int id);
+	boolean deletePerson(int id);
 
-	// Person getPerson(int id);
+	Person getPerson(int id);
 
-	// Person[] getAllPersons();
+	Person[] getAllPersons();
 
 }
