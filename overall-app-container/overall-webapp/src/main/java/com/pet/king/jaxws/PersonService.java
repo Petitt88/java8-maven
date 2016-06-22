a3508d60c739ad42205156b1628ff3e71f4871d8
pkongyik <petitt88@gmail.com>
2016-06-22 04:51:18 +0200

    JAX-WS example

---
+++ overall-app-container/overall-webapp/src/main/java/com/pet/king/jaxws/PersonService.java
@@ -0,0 +1,23 @@
+package com.pet.king.jaxws;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.Style;
+
+//@WebService
+//@SOAPBinding(style = Style.RPC)
+public interface PersonService {
+
+	//@WebMethod
+	String Hello();
+
+	// boolean addPerson(Person p);
+
+	// boolean deletePerson(int id);
+
+	// Person getPerson(int id);
+
+	// Person[] getAllPersons();
+
+}
---
+++ overall-app-container/overall-webapp/src/main/java/com/pet/king/jaxws/PersonServiceImpl.java
@@ -0,0 +1,47 @@
+package com.pet.king.jaxws;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.jws.WebService;
+
+// will be available at "/personws" uri, defined in the web.xml and sun-jaxws.xml files
+//@WebService(endpointInterface = "com.pet.king.jaxws.PersonService")
+public class PersonServiceImpl implements PersonService {
+
+	private static Map<Integer, Person> persons = new HashMap<Integer, Person>();
+
+	public String Hello() {
+		return "Hello from jax-ws webservice";
+	}
+
+	public boolean addPerson(Person p) {
+		if (persons.get(p.getId()) != null)
+			return false;
+
+		persons.put(p.getId(), p);
+		return true;
+	}
+
+	public boolean deletePerson(int id) {
+		if (persons.get(id) == null)
+			return false;
+
+		persons.remove(id);
+		return true;
+	}
+
+	public Person getPerson(int id) {
+		return persons.get(id);
+	}
+
+	public Person[] getAllPersons() {
+		return persons
+				.entrySet()
+				.stream()
+				.map(a -> a.getValue())
+				.collect(Collectors.toList())
+				.toArray(new Person[persons.size()]);
+	}
+}
