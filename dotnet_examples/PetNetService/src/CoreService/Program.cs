961693c51d4f202963e54facab39534479fd49df
Petitt88 <petitt88@gmail.com>
2018-08-14 21:25:54 +0200

    PetNetService: .Net Core sample project with XUnit testing framework

---
+++ dotnet_examples/PetNetService/src/CoreService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoreService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ dotnet_examples/PetNetService/test/CoreService.Tests/CoreServiceTests.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+
+namespace CoreService.Tests
+{
+    public class CoreServiceTests
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Add(2, 2));
+        }
+
+        [Theory]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(7)]
+        public void MyFirstTheory(int value)
+        {
+            Assert.True(IsOdd(value));
+
+            bool IsOdd(int val) => val % 2 == 1;
+        }
+
+        private int Add(int x, int y) => x + y;
+    }
+}
