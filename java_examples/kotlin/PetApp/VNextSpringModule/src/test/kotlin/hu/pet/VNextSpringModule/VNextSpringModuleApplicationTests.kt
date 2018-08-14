b2ac9426715e178e081fe6c361e158938752e9e4
Petitt88 <petitt88@gmail.com>
2018-08-14 20:47:16 +0200

    PetApp: gradle version from variable and swtiching to JUnit 5

--- java_examples/kotlin/PetApp/VNextSpringModule/src/test/kotlin/hu/pet/VNextSpringModule/VNextSpringModuleApplicationTests.kt
+++ java_examples/kotlin/PetApp/VNextSpringModule/src/test/kotlin/hu/pet/VNextSpringModule/VNextSpringModuleApplicationTests.kt
@@ -1,16 +1,15 @@
 package hu.pet.VNextSpringModule
 
-import org.junit.Test
-import org.junit.runner.RunWith
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.context.junit.jupiter.SpringExtension
 
-@RunWith(SpringRunner::class)
+@ExtendWith(SpringExtension::class)
 @SpringBootTest
 class VNextSpringModuleApplicationTests {
 
 	@Test
 	fun contextLoads() {
 	}
-
 }
