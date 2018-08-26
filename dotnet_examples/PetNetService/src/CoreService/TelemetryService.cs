6ebbf5de978c934c163d2551d9b6e1289d559416
Petitt88 <petitt88@gmail.com>
2018-08-26 13:40:33 +0200

    PetService: CoreService became a netstandard class library

---
+++ dotnet_examples/PetNetService/src/CoreService/TelemetryService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace CoreService
+{
+    public class TelemetryService
+    {
+        private readonly ILogger<TelemetryService> _logger;
+
+        public TelemetryService(ILogger<TelemetryService> logger)
+        {
+            _logger = logger;
+        }
+
+        public void Collect()
+        {
+            _logger.LogInformation("Data collected");
+        }
+    }
+}
