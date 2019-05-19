1809366f4710f164a80490faa3728450792e0011
Petitt88 <petitt88@gmail.com>
2019-05-19 21:15:48 +0200

    Flutter changing between front and back camera

--- flutter_examples/BootifulFlutter/lib/widgets/routes/CameraRoute.dart
+++ flutter_examples/BootifulFlutter/lib/widgets/routes/CameraRoute.dart
@@ -22,6 +22,9 @@ class CameraRouteState extends State<CameraRoute> {
   // the Future
   CameraController _controller;
   Future<void> _initCamera;
+  OverlayState _topButtonsOverlay;
+  List<CameraDescription> _cameras;
+  CameraType _cameraType = CameraType.back;
 
   @override
   void initState() {
@@ -32,20 +35,8 @@ class CameraRouteState extends State<CameraRoute> {
   Future<void> _initAsync() async {
     await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
-    final cameras = await availableCameras();
-    final camera = cameras.first;
-
-    // In order to display the current output from the Camera, you need to
-    // create a CameraController.
-    _controller = CameraController(
-      // Get a specific camera from the list of available cameras
-      camera,
-      // Define the resolution to use
-      ResolutionPreset.medium,
-    );
-
-    // Next, you need to initialize the controller. This returns a Future
-    await _controller.initialize();
+    _cameras = await availableCameras();
+    await _changeCamera(CameraType.back);
   }
 
   @override
@@ -53,6 +44,7 @@ class CameraRouteState extends State<CameraRoute> {
     super.dispose();
     // Make sure to dispose of the controller when the Widget is disposed
     _controller?.dispose();
+    _topButtonsOverlay?.dispose();
 
     SystemChrome.setPreferredOrientations([...DeviceOrientation.values]);
   }
@@ -68,30 +60,9 @@ class CameraRouteState extends State<CameraRoute> {
         if (snapshot.connectionState == ConnectionState.done) {
           // If the Future is complete, display the preview
           return Scaffold(
-            body: CameraPreview(_controller),
+            body: _cameraBody(context),
             floatingActionButton: FloatingActionButton(
-              onPressed: () async {
-                // Take the Picture in a try / catch block. If anything goes wrong,
-                // catch the error.
-                try {
-                  // Construct the path where the image should be saved using the path
-                  // package.
-                  final path = join(
-                    // In this example, store the picture in the temp directory. Find
-                    // the temp directory using the `path_provider` plugin.
-                    (await getTemporaryDirectory()).path,
-                    '${DateTime.now()}.png',
-                  );
-
-                  // Attempt to take a picture and log where it's been saved
-                  await _controller.takePicture(path);
-
-                  widget.onPictureTaken(path);
-                  Navigator.pop(context);
-                } catch (e) {
-                  await ToastHelper.error(e.toString());
-                }
-              },
+              onPressed: () => _takePicture(context),
               child: Icon(Icons.camera),
             ),
             floatingActionButtonLocation:
@@ -104,4 +75,93 @@ class CameraRouteState extends State<CameraRoute> {
       },
     );
   }
+
+  Widget _cameraBody(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        CameraPreview(_controller),
+        Positioned(
+          width: MediaQuery.of(context).size.width,
+          child: Container(
+            decoration: BoxDecoration(color: Colors.black),
+            child: Padding(
+              padding: EdgeInsets.only(left: 8, right: 8, top: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(Icons.camera_alt),
+                    color: Colors.white,
+                    onPressed: () => _changeCamera(
+                        _cameraType == CameraType.back
+                            ? CameraType.front
+                            : CameraType.back),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.flash_auto),
+                    color: Colors.white,
+                    onPressed: () {
+                      print("filled background");
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Future<void> _takePicture(BuildContext context) async {
+    // Take the Picture in a try / catch block. If anything goes wrong,
+    // catch the error.
+    try {
+      // Construct the path where the image should be saved using the path
+      // package.
+      final path = join(
+        // In this example, store the picture in the temp directory. Find
+        // the temp directory using the `path_provider` plugin.
+        (await getTemporaryDirectory()).path,
+        '${DateTime.now()}.png',
+      );
+
+      // Attempt to take a picture and log where it's been saved
+      await _controller.takePicture(path);
+
+      widget.onPictureTaken(path);
+      Navigator.pop(context);
+    } catch (e) {
+      await ToastHelper.error(e.toString());
+    }
+  }
+
+  Future<void> _changeCamera(CameraType cameraType) async {
+    // if there is no front camera
+    if (_cameraType == CameraType.front && _cameras.length == 1) return;
+
+    _cameraType = cameraType;
+
+    final camera = _cameraType == CameraType.front
+        ? _cameras.elementAt(1)
+        : _cameras.first;
+
+    // In order to display the current output from the Camera, you need to
+    // create a CameraController.
+    final controller = CameraController(
+      // Get a specific camera from the list of available cameras
+      camera,
+      // Define the resolution to use
+      ResolutionPreset.medium,
+    );
+
+    // Next, you need to initialize the controller. This returns a Future
+    await controller.initialize();
+
+    setState(() {
+      _controller = controller;
+    });
+  }
 }
+
+enum CameraType { back, front }
