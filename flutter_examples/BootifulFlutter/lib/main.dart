f52d6d74313040cd3e3f62761eb168d695f39f59
Petitt88 <petitt88@gmail.com>
2019-05-19 19:32:59 +0200

    Flutter unused imports

--- flutter_examples/BootifulFlutter/lib/main.dart
+++ flutter_examples/BootifulFlutter/lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/widgets/routes/CameraRoute.dart';
 import 'package:flutter_app/widgets/routes/InfiniteListRoute.dart';
-import 'package:flutter_app/widgets/routes/TabsRoute.dart';
 
 void main() => runApp(MyApp());
 
