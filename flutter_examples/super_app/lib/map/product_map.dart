302e0d76b22b2d28463744ee43813cbc670b0ee8
Peter Kongyik <petitt88@gmail.om>
2019-07-18 16:51:00 +0200

    Flutter app: added geolocation package - retrieving the position of the device

--- flutter_examples/super_app/lib/map/product_map.dart
+++ flutter_examples/super_app/lib/map/product_map.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class ProductMap extends StatefulWidget {
@@ -9,30 +10,46 @@ class ProductMap extends StatefulWidget {
 class _ProductMapState extends State<ProductMap> {
   GoogleMapController mapController;
 
-  final LatLng _center = const LatLng(45.521563, -122.677433);
-  bool mapCreated = false;
+  LatLng _userPosition = const LatLng(45.521563, -122.677433);
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
-    this.setState(() => mapCreated = true);
-    mapCreated = true;
   }
 
   @override
   Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _findUserLocation(),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            return _buildUI();
+          default:
+            return Center(
+              child: CupertinoActivityIndicator(
+                animating: true,
+              ),
+            );
+        }
+      },
+    );
+  }
+
+  Widget _buildUI() {
     return Stack(
       children: <Widget>[
         GoogleMap(
           onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(target: _center, zoom: 11),
+          initialCameraPosition: CameraPosition(target: _userPosition, zoom: 13),
+          markers: {Marker(position: _userPosition, markerId: MarkerId("User"))},
         ),
-        if (!mapCreated)
-          Center(
-            child: CupertinoActivityIndicator(
-              animating: true,
-            ),
-          ),
       ],
     );
+  }
+
+  Future<void> _findUserLocation() async {
+    final geolocator = Geolocator();
+    Position position = await geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    _userPosition = LatLng(position.latitude, position.longitude);
   }
 }
