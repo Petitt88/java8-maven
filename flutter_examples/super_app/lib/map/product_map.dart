db3f20b66896527a356542118c2a90a7e75c8cdc
Peter Kongyik <petitt88@gmail.com>
2019-07-18 13:52:08 +0200

    Map component: show activity indicator while being initialized

--- flutter_examples/super_app/lib/map/product_map.dart
+++ flutter_examples/super_app/lib/map/product_map.dart
@@ -10,19 +10,29 @@ class _ProductMapState extends State<ProductMap> {
   GoogleMapController mapController;
 
   final LatLng _center = const LatLng(45.521563, -122.677433);
+  bool mapCreated = false;
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
+    this.setState(() => mapCreated = true);
+    mapCreated = true;
   }
 
   @override
   Widget build(BuildContext context) {
-    return GoogleMap(
-      onMapCreated: _onMapCreated,
-      initialCameraPosition: CameraPosition(
-        target: _center,
-        zoom: 11,
-      ),
+    return Stack(
+      children: <Widget>[
+        GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(target: _center, zoom: 11),
+        ),
+        if (!mapCreated)
+          Center(
+            child: CupertinoActivityIndicator(
+              animating: true,
+            ),
+          ),
+      ],
     );
   }
 }
